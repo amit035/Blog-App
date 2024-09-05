ca0fbac7bc0aa4948036b1ecb89483c17bd45c94
amit035 <amitkumarbehera512@gmail.com>
2024-09-05 15:42:19 +0530

    Adding Navbar Links & Toggle

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import { Button, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useLocation } from 'react-router-dom'
 import { FcSearch } from "react-icons/fc";
 import { LuSunMoon } from "react-icons/lu";
 
 const Header = () => {
+  const path = useLocation().pathname;   
   return (
     <Navbar className='border-b-2'>    
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -21,7 +22,7 @@ const Header = () => {
         <Button className='w-12 h-10 lg:hidden' color='gray' pill>
             <FcSearch/>
         </Button>
-        <div className='flex gap-2'>
+        <div className='flex gap-2 md:order-2'>
             <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                 <LuSunMoon/>
             </Button>
@@ -30,7 +31,25 @@ const Header = () => {
                     Sign In
                 </Button>
             </Link>
+            <Navbar.Toggle/>
         </div>
+            <Navbar.Collapse>
+                <Navbar.Link active={path=== "/"} as={'div'}>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path=== "/about"} as={'div'}>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path=== "/projects"} as={'div'}>
+                    <Link to='/projects'>
+                        Projects
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
     </Navbar>
   )
 }
