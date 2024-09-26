2ba9b3e18b47d9617e426d57496703b881ed4e8c
amit035 <amitkumarbehera512@gmail.com>
2024-09-26 20:08:43 +0530

    Created Sign IN API Routes

--- API/CONTROLLERS/auth.controller.js
+++ API/CONTROLLERS/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../MODELS/user.model.js";
 import bcryptjs from 'bcryptjs';
 import errorMessage from "../UTILS/error.js";
+import jwt from 'jsonwebtoken';
 
 const signup = async (req,res,next) => {
     // console.log(req.body);
@@ -27,4 +28,34 @@ const signup = async (req,res,next) => {
     }
 }
 
-export default signup;
+export {signup};
+
+const signin = async (req,res,next) =>{
+    const {email,password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorMessage(400,'All Fields are Required'));
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorMessage(404,'user not Found'))
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password); 
+        if(!validPassword){
+            return next(errorMessage(400,'Invalid Password'));
+        }
+
+        const token = jwt.sign({id : validUser._id} , process.env.JWT_SECRET);
+        const {password : pass , ...rest} = validUser._doc;
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true
+        }).json(rest);
+    }catch(error){
+        next(error);
+    }
+}
+
+export {signin};
+
--- API/ROUTES/auth.routes.js
+++ API/ROUTES/auth.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import signup from '../CONTROLLERS/auth.controller.js';
+import {signup} from '../CONTROLLERS/auth.controller.js';
+import {signin } from '../CONTROLLERS/auth.controller.js';
 
 const router = express.Router();
+{/*API for Sign Up*/}
 
 router.post('/signup',signup);
+router.post('/signin',signin);
 
 export default router;
