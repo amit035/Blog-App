0bfd6efaf36ffaa6d19ce959b04cc511fb436e7f
amit035 <amitkumarbehera512@gmail.com>
2025-07-02 04:43:13 +0530

    final commit

--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Advertisement from '../Components/Advertisement'
 
 const Projects = () => {
     return(
-        <div>Projects</div>
+        <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+         <h1 className='text-3xl font-semibold'>
+            Projects
+         </h1>
+         <p className='text-base text-gray-500'>
+            Build Fun Projects
+         </p>
+         <Advertisement/>
+        </div>
     )
 }
 
